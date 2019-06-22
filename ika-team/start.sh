cfc586ef70861a60b90819ee24cb5eda9c6e4a6f
Ville Nupponen <ville.nupponen@aika.in>
2019-06-22 16:59:24 +0200

    Fix random-server address

--- ika-team/start.sh
+++ ika-team/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sshfs -o allow_other,StrictHostKeyChecking=no,IdentityFile=~/.ssh/id_rsa,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -p 22 aikain@aika.in:.irssi/log/ika-team/ /var/www/html/logs
+sshfs -o allow_other,StrictHostKeyChecking=no,IdentityFile=~/.ssh/id_rsa,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -p 22 aikain@random.aika.in:.irssi/log/ika-team/ /var/www/html/logs
 
 VOLUME_HOME="/var/lib/mysql"
 
